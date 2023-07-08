52766b5b1319194e5c1e993797fedb0eab24858d
Kirtika Puniani <75201661+KirtikaPuniani@users.noreply.github.com>
2023-07-08 16:44:02 +0530

    Update RollerCoaster2.0.py

--- RollerCoaster2.0.py
+++ RollerCoaster2.0.py
@@ -4,10 +4,10 @@ if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is the age of the person?"))
     if age < 12:
-        print("Price of ticker is 50 Rs")
+        print("Price of ticket is 50 Rs")
     elif age <= 18:     #over 12 and under 18
-        print("Price of ticker is 100 Rs")
+        print("Price of ticket is 100 Rs")
     else:               #everybody else over 18
-        print("Price of ticker is 200 Rs")
+        print("Price of ticket is 200 Rs")
 else:
     print("Apologies! You can't ride the rollercoaster")
